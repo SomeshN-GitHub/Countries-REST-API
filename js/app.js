36b74100cd8d1b017b23d40bbf0a90be9a21e4ab
SomeshN-GitHub <63358076+SomeshN-GitHub@users.noreply.github.com>
2022-01-02 21:12:15 +0530

    setTimeout set to 100ms from 50ms

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ async function displayCountries(){
                 </div>
             </div>
             `; 
-        }, 50);
+        }, 100);
 
         // populate regions in dropdown filter button     
         if(!filterButton.innerHTML.includes(element.region)){    
